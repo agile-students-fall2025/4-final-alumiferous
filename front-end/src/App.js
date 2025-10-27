b27875a7b4fba2e877c907f58af303f9957dc0d1
ajokt123 <at4933@nyu.edu>
2025-10-28 00:32:23 +0100

    added login/signup routes

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,10 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './Login';
 import './App.css';
 import Header from './Header';
 import Home from './Home';
 import Footer from './Footer';
+import Login from './Login';
+import Signup from './Signup';
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
         <Footer />
       </BrowserRouter>
