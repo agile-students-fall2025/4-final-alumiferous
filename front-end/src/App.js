fdbbcd2ea227e61f34fd70c22ef4c6c41a50c060
GharbinBern <bg2696@nyu.edu>
2025-10-29 00:08:11 -0500

    Revert "Added Settings, Reset Password, Report Problem, and Delete Account pages with dark mode support"
    This reverts commit 5bda407e047672e1b9815076a8ecfeec04e25318.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,112 +5,59 @@ import Home from './Home';
 import Footer from './Footer';
 import Login from './Login';
 import Signup from './Signup';
-import Profile from './Profile';
+import Profile from './Profile'
 import EditProfile from './EditProfile';
+
 import SkillDescription from './SkillDescription';
-import Settings from './Settings';
-import ResetPassword from './ResetPassword';
-import ReportProblem from './ReportProblem';
-import DeleteAccount from './DeleteAccount';
+
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <BrowserRouter>
         <Routes>
-          {/* Login / Signup */}
           <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
+          
           <Route path="/signup" element={<Signup />} />
 
-          {/* Home */}
           <Route
             path="/home"
             element={
               <>
                 <Header />
                 <Home />
-                {/* <Footer /> */}
+                {/*<Footer />*/}
               </>
             }
           />
 
-          {/* Profile */}
           <Route
             path="/profile"
             element={
               <>
                 <Header />
                 <Profile />
-                {/* <Footer /> */}
+                {/*<Footer />*/}
               </>
             }
           />
 
-          {/* Edit Profile */}
           <Route
             path="/edit-profile"
             element={
               <>
                 <Header />
                 <EditProfile />
-                {/* <Footer /> */}
+                {/*<Footer />*/}
               </>
             }
           />
 
-          {/* Skill Description */}
-          <Route path="/skill/:id" element={<SkillDescription />} />
-
-          {/* Settings */}
-          <Route
-            path="/settings"
-            element={
-              <>
-                <Header />
-                <Settings />
-                <Footer />
-              </>
-            }
-          />
-
-          {/* Reset Password */}
-          <Route
-            path="/reset-password"
-            element={
-              <>
-                <Header />
-                <ResetPassword />
-                <Footer />
-              </>
-            }
-          />
-
-          {/* Report Problem */}
-          <Route
-            path="/report-problem"
-            element={
-              <>
-                <Header />
-                <ReportProblem />
-                <Footer />
-              </>
-            }
-          />
-
-          {/* Delete Account */}
-          <Route
-            path="/delete-account"
-            element={
-              <>
-                <Header />
-                <DeleteAccount />
-                <Footer />
-              </>
-            }
-          />
+          
+          <Route path="/skill/:id" element={<SkillDescription/>} />
         </Routes>
-        {/* <Footer /> */}
+        {/*<Footer />*/}
       </BrowserRouter>
     </div>
   );
