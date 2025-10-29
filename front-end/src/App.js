de6e2f225819e44e4a9171d68bdc87c784bc7854
aa10699Alisha <alishaatif@10-16-75-92.dynapool.wireless.nyu.edu>
2025-10-29 03:53:07 -0400

    Updated app.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -109,7 +109,7 @@ export default function App() {
                 <Footer />
               </>
             }
-          /> */}
+          /> 
           {/* <Route
             path="/report-problem"
             element={
